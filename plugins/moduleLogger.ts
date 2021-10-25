560363e3c505c821a5ed91ad7e512d960cb35696
Alexander Mamutov <a.mamutov@maugry.ru>
2021-10-25 12:43:12 +0400

    Update plugin: add exclude option, make it more async, use Set for result

--- plugins/moduleLogger.ts
+++ plugins/moduleLogger.ts
@@ -3,17 +3,23 @@ import glob from 'glob'
 import * as path from 'path';
 import * as fs from 'fs';
 
+type OptionsType = {
+    srcRoot?: string,
+    output?: string,
+    exclude?: RegExp[];
+}
+
 class ModuleLogger {
+    options: OptionsType;
+    files: Set<string>;
 
-    static defaultOptions = {
-        srcRoot: path.resolve(__dirname, '../src'),
-        output: path.resolve(__dirname, '../unused')
-    };
-
-    options;
-
-    constructor(options = {}) {
-        this.options = { ...ModuleLogger.defaultOptions, ...options };
+    constructor(options: OptionsType = {}) {
+        this.options = {
+            srcRoot: path.resolve(__dirname, '../src'),
+            output: path.resolve(__dirname, '../unused'),
+            exclude: [],
+            ...options
+        };
       }
 
     apply(compiler: Compiler) {
@@ -25,25 +31,35 @@ class ModuleLogger {
                 normalModuleFactory.hooks.module.tap('ModuleLogger', (_module, _createData, resolveData) => {
                     // @ts-ignore
                     const modulePath = _createData.resource;
+
                     if (modulePath.startsWith(this.options.srcRoot)) {
                         usedImports.add(modulePath)
+                        this.files.delete(modulePath)
                     }
 
                     return _module;
                 });
-
-
             }
         );
 
-        compiler.hooks.done.tap('ModuleLogger', () => {
+        compiler.hooks.beforeRun.tapAsync('ModuleLogger', ({}, callback) => {
             glob(this.options.srcRoot + '/**/*', { nodir: true }, (err, files) => {
-                const result = files
+                this.files = new Set()
+                files
                     .map((file) => path.resolve(__dirname, '..' ,file))
-                    .filter(file => !usedImports.has(file))
+                    .filter(file => !this.options.exclude.some(regexp => {
+                        return regexp.test(file)
+                    }))
+                    .forEach(file => this.files.add(file))
                 
-                fs.writeFileSync(this.options.output, JSON.stringify(result, null, 2))
+                callback()
             });
+        })
+
+        compiler.hooks.done.tapAsync('ModuleLogger', ({}, callback) => {
+            const result = Array.from(this.files)
+            fs.writeFileSync(this.options.output, JSON.stringify(result, null, 2))
+            callback()
         });
     }
 }
